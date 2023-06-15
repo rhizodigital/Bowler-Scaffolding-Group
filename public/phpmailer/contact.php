038c6e2a0d09c5c63fa07ad2b8dc83d7a00e5413
Michael Sables <rhizodigital@gmail.com>
2023-06-15 11:02:52 +0100

    Add phpmailer

---
+++ public/phpmailer/contact.php
@@ -0,0 +1,104 @@
+<?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
+// Settings
+// Who message will be sent from. Use a fixed address in your own domain or 
+// it can cause your message to fail SPF checks
+$fromEmail = 'michael.sables@omnific.studio';
+$fromName = 'Michael Sables';
+// Who message will be sent to
+$toEmail = 'rhizodigital@gmail.com';
+$toName = 'Michael Sables';
+// The message subject line
+$subject = 'Hello from this website';
+
+
+// SMTP Settings
+
+// Hostname of SMTP server
+$smtpHost = 'smtppro.zoho.eu';
+// Set the SMTP port number - likely to be 25, 465 or 587
+$smtpPort = 465;
+// Username for SMTP Autentication
+$smtpUser = 'michael.sables@omnific.studio';
+// Password for SMTP Authentication
+$smtpPass = 'AYvDNMyhCTEa';
+$smtpSecure = 'ssl'; // Enable TLS or SSL encryption
+$smtpAutoTLS = false; // Enable Auto TLS
+
+//======================================================
+    if (isset($_POST)) {
+    $data = json_decode(file_get_contents('php://input'), true);
+    }
+
+// Dont run unless we have a email
+if (array_key_exists('email', $data)) {
+
+    $isAjax = !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+
+    // Create PHPMailer Instance
+    $mail = new PHPMailer();
+    // Tell PHPMailer to use SMTP;
+    $mail->isSMTP();
+
+    $mail->Host = $smtpHost;
+    $mail->Port = $smtpPort;
+    $mail->Password = $smtpPass;
+    $mail->Username = $smtpUser;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = $smtpSecure;
+    $mail->SMTPAutoTLS = $smtpAutoTLS;
+    $mail->setFrom($fromEmail, $fromName);
+    $mail->addAddress($toEmail, $toName);
+
+    if ($mail->addReplyTo($data['email'], $data['name'])) {
+
+        $mail->CharSet = 'UTF-8';
+        $mail->isHTML(false);
+        $mail->Subject = $subject;
+        $mail->Body = <<<EOT
+Name: {$data['name']}
+Email: {$data['email']}
+Message: {$data['message']}
+Phone: {$data['phone']}
+EOT;
+
+        //send the message, check for errors
+        if (!$mail->send()) {
+            if ($isAjax) {
+                http_response_code(500);
+            }
+
+            $response = [
+                "status" => "failed",
+                "message" => 'Sorry, something went wrong, Please try again later.'
+            ];
+        } else {
+            $response = [
+                "status" => "success",
+                "message" => 'Message sent! Thanks for contacting us.'
+            ];
+        };
+
+    } else {
+        $response = [
+            "status" => "failed",
+            "message" => 'Invalid email address, message ignored'
+        ];
+    }
+
+    if ($isAjax) {
+        header('Content-type:application/json;charset=utf-8');
+        echo json_encode($response);
+        exit();
+    }
+    
+}
+
+?>
