1661786d85b11b0b2167682903712fe0ee115727
Michael Sables <rhizodigital@gmail.com>
2024-10-22 10:38:50 +0100

    Optimised images, add meta descriptions per page, add analytics code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://www.miltonkeynesscaffolding.co.uk",
+  integrations: [tailwind(), sitemap()],
   vite: {
     ssr: {
-      external: ["svgo"]
-    }
-  }
+      external: ["svgo"],
+    },
+  },
 });
