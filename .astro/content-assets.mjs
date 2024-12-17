308dc52bc70e17774da8ecaab42646c206f33db2
Michael Sables <rhizodigital@gmail.com>
2024-12-17 14:00:37 +0000

    Clear astro folder

--- .astro/content-assets.mjs
+++
@@ -1,7 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_ZsHdjc from "./michael-sables.jpg?astroContentImageFlag=&importer=src%2Fdata%2Fauthors%2Fauthors.json";
-import __ASTRO_IMAGE_IMPORT_Z1PIKYQ from "_images/layher-as-1.jpg?astroContentImageFlag=&importer=src%2Fdata%2Fnews%2Fwhy-we-use-layher-scaffolding-how-it-benefits-you.mdx";
-import __ASTRO_IMAGE_IMPORT_ZPJCYh from "domestic-solar-job-1.jpg?astroContentImageFlag=&importer=src%2Fdata%2FcaseStudies%2Fscaffold-for-solar-panels-install-milton-keynes%2Findex.mdx";
-import __ASTRO_IMAGE_IMPORT_Z1T6LI1 from "_images/new-builds.jpg?astroContentImageFlag=&importer=src%2Fdata%2Fnews%2Flabours-housing-plan-boost-scaffolding-construction-industry.mdx";
-export default new Map([["./michael-sables.jpg?astroContentImageFlag=&importer=src%2Fdata%2Fauthors%2Fauthors.json", __ASTRO_IMAGE_IMPORT_ZsHdjc], ["_images/layher-as-1.jpg?astroContentImageFlag=&importer=src%2Fdata%2Fnews%2Fwhy-we-use-layher-scaffolding-how-it-benefits-you.mdx", __ASTRO_IMAGE_IMPORT_Z1PIKYQ], ["domestic-solar-job-1.jpg?astroContentImageFlag=&importer=src%2Fdata%2FcaseStudies%2Fscaffold-for-solar-panels-install-milton-keynes%2Findex.mdx", __ASTRO_IMAGE_IMPORT_ZPJCYh], ["_images/new-builds.jpg?astroContentImageFlag=&importer=src%2Fdata%2Fnews%2Flabours-housing-plan-boost-scaffolding-construction-industry.mdx", __ASTRO_IMAGE_IMPORT_Z1T6LI1]]);
-		
--- .astro/content-modules.mjs
+++
@@ -1,6 +0,0 @@
-
-export default new Map([
-["src/data/news/why-we-use-layher-scaffolding-how-it-benefits-you.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fnews%2Fwhy-we-use-layher-scaffolding-how-it-benefits-you.mdx&astroContentModuleFlag=true")],
-["src/data/caseStudies/scaffold-for-solar-panels-install-milton-keynes/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2FcaseStudies%2Fscaffold-for-solar-panels-install-milton-keynes%2Findex.mdx&astroContentModuleFlag=true")],
-["src/data/news/labours-housing-plan-boost-scaffolding-construction-industry.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fnews%2Flabours-housing-plan-boost-scaffolding-construction-industry.mdx&astroContentModuleFlag=true")]]);
-		
